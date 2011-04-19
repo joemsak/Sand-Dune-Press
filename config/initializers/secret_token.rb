a62d53676eea7049f95cb082a533a1f1b904f797
Erik Schneider <schnee41@gmail.com>
2011-04-18 20:29:16 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sanddunepress::Application.config.secret_token = '7e9e665138c26edea20f7f279bc3094ffb1dd6a96fb66b39925725da0183843c96df3bb15fbca1ecccbdd3abcf257335f6d242c7db32b509bfc6779942028da1'
