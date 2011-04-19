46537f1fedc55fea6f15071178e0fa0ed12886d4
Erik Schneider <schnee41@gmail.com>
2011-04-18 21:25:38 -0500

    fixed compass path and updated base stylesheets

--- config/compass.rb
+++ config/compass.rb
@@ -3,5 +3,5 @@ project_path = Compass::AppIntegration::Rails.root
 environment  = Compass::AppIntegration::Rails.env
 
 http_path = '/'
-css_dir   = 'tmp/stylesheets'
+css_dir   = 'tmp/stylesheets/compiled'
 sass_dir  = 'app/stylesheets'
