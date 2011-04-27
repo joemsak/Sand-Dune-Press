7c6d2a70fcd0734a9d5b2555e2691b0978585c42
Erik Schneider <schnee41@gmail.com>
2011-04-26 21:52:03 -0500

    s3 edits

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,5 +46,7 @@ Sanddunepress::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  Refinery.s3_backend = true
 end
 Refinery.rescue_not_found = true
