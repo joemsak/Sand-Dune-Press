1b790dbcc68208e38acff52ae33bea011ce3a1bb
Joe Sak <joe@joesak.com>
2011-05-06 15:19:12 -0700

    Edited app/controllers/comic_pages_controller.rb via GitHub

--- app/controllers/comic_pages_controller.rb
+++ app/controllers/comic_pages_controller.rb
@@ -4,7 +4,7 @@ class ComicPagesController < ApplicationController
 
   def show
     @comic_page = ComicPage.find(params[:id])
-    render "/comics/comic_pages", :comic_page => @comic_page
+    render :json => @comic_page
   end
 
 
